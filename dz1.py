90511420767bf130e88877551ebd0d2ef9a9c742
nixsilvam <81374946+nixsilvam@users.noreply.github.com>
2022-04-07 11:37:55 +0300

    Add files via upload

---
+++ dz1.py
@@ -0,0 +1,19 @@
+def list_function(f_t_d):
+    def the_wrapper(x):
+        f_t_d(x)
+        return x.split()
+    return the_wrapper
+
+
+@list_function
+def return_string(x):
+    return x
+
+
+def user_input():
+    inp = input("Enter a text string: ")
+    return return_string(inp)
+
+
+if __name__ == '__main__':
+    print(user_input())
